ac765ff1818a45b866866295ecea4de987f2d195
gyotaek <leegyotaek@gmail.com>
2014-12-27 10:19:18 +0900

    static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
+
 root 'static_pages#home'
+get 'signup' => 'users#new'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-     assert_select "title", "Home|LanguageApp"
+     assert_select "title", "LanguageApp|Home"
   end
 
 end
