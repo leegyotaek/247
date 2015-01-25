34e4ea8dcdfd699d11697e75acffefe9f84b0ca8
gyotaek <leegyotaek@gmail.com>
2015-01-26 02:48:27 +0900

    change timestamps to datetime

--- db/migrate/20150123085828_add_columns_to_friendships.rb
+++ db/migrate/20150123085828_add_columns_to_friendships.rb
@@ -2,6 +2,6 @@ class AddColumnsToFriendships < ActiveRecord::Migration
   def change
     add_column :friendships, :status, :string
     add_index :friendships, :status
-    add_column :friendships, :accepted_at, :timestamps
+    add_column :friendships, :accepted_at, :datetime
   end
 end
