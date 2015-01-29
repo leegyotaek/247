464bf1da5bd7d1af1ff1122571d41d2e2d770e6c
gyotaek <leegyotaek@gmail.com>
2015-01-29 17:36:55 +0900

    매칭 컬럼 추가

---
+++ db/migrate/20150129080835_addn_matching_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddnMatchingColumnToUsers < ActiveRecord::Migration
+  def change
+  add_column :users , :matching_lan , :string
+  add_column :users , :matching_age_from , :integer
+  add_column :users , :matching_age_to , :integer
+  add_column :users , :matching_interest , :string
+  end
+end
