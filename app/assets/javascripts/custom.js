3f7392b50840e6c8871aa883a418e8f16224844c
sukyumKim <ikks06luck@naver.com>
2015-02-22 20:56:38 +0900

    test

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,5 @@
 //= require cocoon
 //= require webshims/polyfiller
-$.webshims.setOptions('basePath', '/webshims/shims/')
-$.webshims.setOptions('waitReady', false);
-$.webshims.setOptions('forms-ext', {types: 'date'});
-$.webshims.polyfill('forms forms-ext');
+webshims.setOptions('basePath', '/webshims/shims/');
+webshims.setOptions('forms-ext', {types: 'date'});
+webshims.polyfill('forms forms-ext');
