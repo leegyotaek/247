4a25c37af28562d3ed525ee69081879b2f5278bd
sukyumKim <ikks06luck@naver.com>
2015-02-23 17:55:27 +0900

    modi

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,6 @@
 //= require cocoon
 //= require webshims/polyfiller
 webshims.setOptions('basePath', '/webshims/shims/');
+webshims.setOptions('waitready', false);
 webshims.setOptions('forms-ext', {types: 'date'});
 webshims.polyfill('forms forms-ext');
