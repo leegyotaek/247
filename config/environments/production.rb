7bce85c851bf9eafbc0aec47615fb73a293fd7dc
sukyumKim <ikks06luck@naver.com>
2015-04-07 12:18:35 +0900

    minify and compress test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,27 +45,30 @@ Rails.application.configure do
   config.middleware.use Rack::Deflater
   config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
 
-  config.middleware.use HtmlCompressor::Rack,
-    compress_css: true,
-    compress_javascript: true,
-    css_compressor: Sass,
-    enabled: true,
-    javascript_compressor: uglifier,
-    preserve_line_breaks: false,
-    remove_comments: true,
-    remove_form_attributes: false,
-    remove_http_protocol: false,
-    remove_https_protocol: false,
-    remove_input_attributes: true,
-    remove_intertag_spaces: false,
-    remove_javascript_protocol: true,
-    remove_link_attributes: true,
-    remove_multi_spaces: true,
-    remove_quotes: true,
-    remove_script_attributes: true,
-    remove_style_attributes: true,
-    simple_boolean_attributes: true,
-    simple_doctype: false
+  config.middleware.use HtmlCompressor::Rack, options
+
+  options = {
+    :compress_css => true,
+    :compress_javascript => true,
+    :css_compressor => :sass,
+    :enabled => true,
+    :javascript_compressor => uglifier,
+    :preserve_line_breaks => false,
+    :remove_comments => true,
+    :remove_form_attributes => false,
+    :remove_http_protocol  =>false,
+    :remove_https_protocol  =>false,
+    :remove_input_attributes  =>true,
+    :remove_intertag_spaces => false,
+    :remove_javascript_protocol => true,
+    :remove_link_attributes => true,
+    :remove_multi_spaces => true,
+    :remove_quotes => true,
+    :remove_script_attributes => true,
+    :remove_style_attributes => true,
+    :simple_boolean_attributes => true,
+    :simple_doctype => false
+  }
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
